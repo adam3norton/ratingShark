d9dd96589679a49dadd7f9274bf0162e12188649
Tanner Woodrum <tanner.woodrum1@gmail.com>
2022-12-13 23:25:46 -0700

    fix?

--- RatingShark/settings.py
+++ RatingShark/settings.py
@@ -79,6 +79,16 @@ TEMPLATES = [
     },
 ]
 
+# Find templates in the same folder as settings.py.
+SETTINGS_PATH = os.path.realpath(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
+
 WSGI_APPLICATION = 'RatingShark.wsgi.application'
 
 
