1582bbf20f014a4c5d364069be7f859666996f26
Tanner Woodrum <tanner.woodrum1@gmail.com>
2022-12-13 23:33:07 -0700

    fixxxxx

--- RatingShark/settings.py
+++ RatingShark/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'RatingShark.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.railway.RailwayTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
